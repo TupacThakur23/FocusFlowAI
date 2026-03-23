50845197beeb5b9d9878863a792d1cf92ffc38ba
TupacThakur23 <yuvrajsingh18october@gmail.com>
2026-03-23 19:14:12 +0530

    Fixed UI Bugs

--- content.js
+++ content.js
@@ -1,17 +1,23 @@
 console.log("Content script running");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === "GET_DATA") {
 
-        const data = {
-            title: document.title,
-            url: window.location.href,
+    if (request.type !== "GET_DATA") return;
 
-            headings: Array.from(document.querySelectorAll("h1,h2,h3")).map(el => el.innerText),
+    const data = {
+        title: document.title,
+        url: window.location.href,
 
-            links: Array.from(document.querySelectorAll("a")).map(a => a.href)
-        }
-    }
-    
+        headings: Array.from(document.querySelectorAll("h1,h2,h3"))
+            .map(el => el.innerText),
+
+        links: Array.from(document.querySelectorAll("a"))
+            .map(a => a.href)
+    };
+
+    console.log("SENDING DATA:", data); 
+
     sendResponse(data);
+
+    return true;
 });
